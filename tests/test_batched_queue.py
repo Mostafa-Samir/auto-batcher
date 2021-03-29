f0055ca10c73f316fee22a32174574405b9ee909
Mostafa Samir <mostafa.3210@gmail.com>
2021-03-29 23:41:51 +0200

    add failing test for windows to check workflow

--- tests/test_batched_queue.py
+++ tests/test_batched_queue.py
@@ -1,6 +1,6 @@
 import queue
 import unittest
-
+import os
 from auto_batcher.batched_queue import BatchedQueue
 
 class BatchedQueueTest(unittest.TestCase):
@@ -9,7 +9,7 @@ class BatchedQueueTest(unittest.TestCase):
         """Tests the `get_batch` method of the batched queue
         """
         bqueue = BatchedQueue()
-        
+
         for i in range(5):
             bqueue.put(i)
 
@@ -17,6 +17,9 @@ class BatchedQueueTest(unittest.TestCase):
         self.assertListEqual(bqueue.get_batch(up_to=3), [3, 4])
         with self.assertRaises(queue.Empty):
             bqueue.get_batch(up_to=3, timeout=0.05)
+    
+        if os.name == 'nt':
+            self.fail("Failing on windows")
 
     def test_put_batch(self):
         """Tests the `put_batch` method of the batched queue
